39fceb0d6d25dd33c90451b16f0bdbcee9e328a2
ravi13ravi <123901102+ravi13ravi@users.noreply.github.com>
2025-10-15 23:45:37 +0530

    Add files via upload

---
+++ frontend/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import SymptomChecker from './components/SymptomChecker';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <SymptomChecker />
+    </div>
+  );
+}
+
+export default App;
